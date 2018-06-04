e3c46923d7690025d377d3ae95a1671e559ab142
Gary Feierbach <gf@inneraccess.com>
2018-06-04 16:08:13 -0700

    Add files via upload

---
+++ Section2/Julia1_2.4.jl
@@ -0,0 +1,57 @@
+# Julia1_2.4.jl Operators and Elementary Math Functions
+#
+#Operators + _ * / \ ^
+a=2//3 + 3.4*π/e
+b=(3//4)^2 - 55\5  # x=a\b is x=b/a
+
+V=[1 2 3]    # vector
+3V
+
+M=[1 2; 3 4] # 2x2 matrix
+Y=3M         # * implied, 3* each entry
+
+Z1=M/3  # matrix M divided by 3
+Z2=3\M  # same result
+
+W1=(3+3im)\M
+W2=(3+3im)W1
+# op= Operators
+i=0
+i+=1
+M*=2 # Stays integer, all antries times 2
+M-=1 # Stays integer, -1 from each entry
+M*=2.0 # now floating, all entries * 2.0
+# Special functions for complex numbers
+real(1+2im)
+imag(1+2im)
+conj(1+2im)
+
+# Basic function library
+d=3+3im
+a=sqrt(d)
+a^2
+
+# notice sqrt(-1) gives an error
+sqrt(-1)
+# but sqrt(-1 + 0im) works
+
+a1=abs(1+2im)
+a2=abs2(1+2im)
+a3=sqrt(-1+0im)
+a4=sqrt(-1+2im)
+a5=sin(1+2im)
+a6=cosh(1+2im)
+a7=exp(1+2im)
+a8=log(1+2im)
+
+# Miscellaneous functions for Ints and Floats
+# also round(x), floor(x), ceil(x), trunc(x),
+# rem(x,y), mod(x,y), gcd(x,y), lcm(x,y) and many variations
+# also hypot(x,y) hypotenuse, cbrt(x) cube root,
+# various log  and exp functions
+println(ceil(4.3))
+println(round(4.3))
+println(floor(4.3))
+println(hypot(3,4))
+println(cbrt(27))
+#
---
+++ Section2/Julia1_2.5.jl
@@ -0,0 +1,42 @@
+# Julia1_2.5.jl Strings (Unicode and UTF-8)
+#
+# ASCII is preserved in the first 128
+#  characters of UTF-8 so only 1 byte necessary
+# A UTF-8 encoding can take 1 to 4 bytes in a UTF-8 String
+#  to get the size in bytes:
+sizeof("∑")  # ∑ is upper case sigma
+
+s1="abcπ∑d" # UTF-8 String type
+println(sizeof(s1))   # Number of bytes
+
+hg=80
+s2=string("xy",3,Char(0x3B1),false,s1,hg) # function converts UTF-8
+println(s2)
+println(sizeof(s2))   # Number of bytes
+println(endof(s1))    # Byte address of last character
+println(length(s1))   # number of characters
+# translate a UTF-8 string to a vector of UInt8,
+#  UInt16, UInt32 or vice-versa
+s="abcde"
+t=transcode(UInt8,s)
+
+a1=ascii(s)  # Will convert to an ascii string or error
+#    if non-ascii (used to send strings to a C routine)
+a1=ascii(s1)
+
+println(s1)
+a2=transcode(UInt32,s1)  # can make 32-bit Char Vector.
+println(a2)
+println(sizeof(a2))   # Number of bytes
+println(endof(a2))    # word address of last character
+println(length(a2))   # number of 32-bit character
+
+
+# Char() can be used to check out unicode characters to see
+#    if they are what you want
+#  I always use the hexadecimal representations of the Unicode
+Char(0x3B3)
+hex('γ')  # to see the hex Unicode for a character
+Char(0x3C6)
+Char(0x3C9)  # Greek alphabet 0x3B1 through 0x3C9.
+Char(0x263A)
