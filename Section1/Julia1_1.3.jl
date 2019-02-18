c174db3d9d8c1d6895f5d87f2e2108427cc98677
Gary Feierbach <gf@inneraccess.com>
2019-02-17 21:13:02 -0800

    Add files via upload

--- Section1/Julia1_1.3.jl
+++ Section1/Julia1_1.3.jl
@@ -10,10 +10,16 @@ println("Please give me a name for I have none.")
 name=readline()
 # Is it a bad name or a good name
 n=get(badnames,name,0)
-if n==1 println("I think that's not a very nice name.")
+if n==0
+    println("$name is nice, I will take it." )
+else
+    println("I think that's not a very nice name.")
     name="Sky"  #could use a random name selector here
     println("I chose the name $name for myself.")
     println("$name is a strong name and I like it.")
-else
-    println("$name is nice, I will take it." )
 end
+# after this you could read in sentences
+# have a verb and noun dictionary
+# match on the verb and the noun and do stuff
+# or do the psychiatrist thing and turn their
+# responses back into questions.
